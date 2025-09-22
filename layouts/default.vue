5d4cd16178cc0d816ef6c2589d613f0ac29cd5dc
huangwei <1399852582@qq.com>
2025-09-22 16:57:50 +0800

    实施 use_id

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,9 +5,21 @@
 </template>
 
 <script>
+import { v4 as uuidv4 } from "uuid";
 export default {
   mounted() {
     this.getTTClid();
+    try {
+      let userId = sessionStorage.getItem("userId");
+      if (!userId) {
+        userId = uuidv4().replace(/-/g, "");
+        sessionStorage.setItem("userId", userId);
+      }
+      window.dataLayer = window.dataLayer || []
+      window.dataLayer.push({
+        'user_id': userId
+      });
+    } catch (e) {}
   },
   methods: {
     getTTClid() {
